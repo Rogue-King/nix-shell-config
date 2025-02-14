6d159b312c9f8ff7c1f6d11728e5c24df288d392
rogueking <miguel@muniz.org>
2025-02-14 00:54:10 -0800

    fixed update edge case

--- update.sh
+++ update.sh
@@ -2,6 +2,18 @@
 
 git pull
 
+files=("home-manager/home.nix" "home-manager/cli-tools.nix")
+
+for file in "${files[@]}"; do
+    if [ -f "$file" ]; then
+        # Use sed to replace 'USER' with the replacement word
+        sed -i "s/USER/$USER/g" $file
+        echo "Processed: $file"
+    else
+        echo "Error: $file does not exist or is not a regular file."
+    fi
+done
+
 cp -r home-manager/ ~/.config/
 
 home-manager switch
